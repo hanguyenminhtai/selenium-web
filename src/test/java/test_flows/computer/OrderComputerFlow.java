3a8bd394f18548e02390e2b2806530d82934c6d3
ha tai <hanguyenminhtai@gmail.com>
2023-09-20 23:29:10 +0700

    JSON -> Java Object Data

--- src/test/java/test_flows/computer/OrderComputerFlow.java
+++ src/test/java/test_flows/computer/OrderComputerFlow.java
@@ -3,21 +3,24 @@ package test_flows.computer;
 import models.components.order.ComputerEssentialComponent;
 import models.pages.ComputerItemDetailsPage;
 import org.openqa.selenium.WebDriver;
+import test_data.computer.ComputerData;
 
 public class OrderComputerFlow<T extends ComputerEssentialComponent> {
 
     private final WebDriver driver;
     private final Class<T> computerEssentialComponent;
+    private ComputerData computerData;
 
-    public OrderComputerFlow(WebDriver driver, Class<T> computerEssentialComponent) {
+    public OrderComputerFlow(WebDriver driver, Class<T> computerEssentialComponent, ComputerData computerData) {
         this.driver = driver;
         this.computerEssentialComponent = computerEssentialComponent;
+        this.computerData = computerData;
     }
 
     public void buildCompSpecAndAddToCart(){
         ComputerItemDetailsPage computerItemDetailsPage = new ComputerItemDetailsPage(driver);
         T computerEssentialComp = computerItemDetailsPage.computerComp(computerEssentialComponent);
-        computerEssentialComp.selectProcessorType("2.5GHz");
-        computerEssentialComp.selectRAMType("4GB");
+        computerEssentialComp.selectProcessorType(computerData.getProcessorType());
+        computerEssentialComp.selectRAMType(computerData.getRam());
     }
 }
--- src/test/java/tests/computer/BuyingStandardComputerTest.java
+++ src/test/java/tests/computer/BuyingStandardComputerTest.java
@@ -1,19 +1,28 @@
 package tests.computer;
 
 import models.components.order.StandardComputerComponent;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+import test_data.DataObjectBuilder;
+import test_data.computer.ComputerData;
 import test_flows.computer.OrderComputerFlow;
 import tests.BaseTest;
 import url.Urls;
 
 public class BuyingStandardComputerTest extends BaseTest implements Urls {
 
-    @Test
-    public void testStandardComputerBuying(){
+    @Test(dataProvider = "computerData")
+    public void testStandardComputerBuying(ComputerData computerData){
         driver.get(demoBaseUrl.concat("/build-your-own-computer"));
         OrderComputerFlow<StandardComputerComponent> orderComputerFlow =
-                new OrderComputerFlow<>(driver, StandardComputerComponent.class);
+                new OrderComputerFlow<>(driver, StandardComputerComponent.class, computerData);
 
         orderComputerFlow.buildCompSpecAndAddToCart();
+    }
+
+    @DataProvider
+    public ComputerData[] computerData(){
+        String fileLocation = "/src/test/java/test_data/computer/StandardComputerDataList.json";
+        return DataObjectBuilder.buildDataObjectFrom(fileLocation, ComputerData[].class);
     }
 }
